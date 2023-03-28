2f101b5b1a97f9659692f5c78b9a876c3c67f4b5
Pavaneswar630 <112718229+Pavaneswar630@users.noreply.github.com>
2023-03-28 13:56:10 +0530

    Add files via upload

--- prime numbers.cpp
+++ prime numbers.cpp
@@ -1,19 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+ 
 int main()
 {
-	int n,i,m=0,flag=0;
-	printf("enter the number");
-	scanf("%d",&n);
-	m=n%2;
-	for(i=2;i<=m;i++);
-	{
-		if(n%i==0)
-		{
-		  printf("number is not prime");
-		  flag=1;
-		}
-   } 
-   if(flag==0)
-   printf("number is prime");
-   return 0;
+    int num1, num2, i, j, flag, temp, count = 0;
+ 
+    printf("Enter the value of num1 and num2 \n");
+    scanf("%d %d", &num1, &num2);
+    if (num2 < 2)
+    {
+        printf("There are no primes upto %d\n", num2);
+        exit(0);
+    }
+    printf("Prime numbers are \n");
+    temp = num1;
+    if ( num1 % 2 == 0)
+    {
+        num1++;
+    }
+    for (i = num1; i <= num2; i = i + 2)
+    {
+        flag = 0;
+        for (j = 2; j <= i / 2; j++)
+        {
+            if ((i % j) == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0)
+        {
+            printf("%d\n", i);
+            count++;
+        }
+    }
+    printf("Number of primes between %d & %d = %d\n", temp, num2, count);
+return 0;
 }
